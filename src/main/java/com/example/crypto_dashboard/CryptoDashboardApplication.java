7a928a02113e126ee741812b38da29d3859c0915
postwo <codo7717@naver.com>
2025-04-22 19:24:56 +0900

    세팅

---
+++ src/main/java/com/example/crypto_dashboard/CryptoDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.crypto_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crypto_dashboard/CryptoDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crypto_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
